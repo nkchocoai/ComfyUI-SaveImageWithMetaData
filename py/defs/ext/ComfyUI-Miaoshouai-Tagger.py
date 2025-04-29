9ad45683c7ea1566965de481d7bfec04f286ff19
Light-x02 <105165695+Light-x02@users.noreply.github.com>
2025-04-29 03:24:43 +0200

    Add files via upload

---
+++ py/defs/ext/ComfyUI-Miaoshouai-Tagger.py
@@ -0,0 +1,22 @@
+# https://github.com/miaoshouai/ComfyUI-Miaoshouai-Tagger
+from ..meta import MetaField
+from ..formatters import calc_model_hash, calc_lora_hash, convert_skip_clip
+
+SAMPLERS = {
+}
+
+CAPTURE_FIELD_LIST = {
+    "Miaoshouai_Tagger": {
+        MetaField.MODEL_NAME:    {"field_name": "model"},
+    },
+    "Miaoshouai_SaveTags": {
+        MetaField.POSITIVE_PROMPT: {"field_name": "captions"},
+    },
+    "Miaoshouai_Flux_CLIPTextEncode": {
+        MetaField.POSITIVE_PROMPT: {"field_name": "caption"},
+        MetaField.CFG:             {"field_name": "guidance"},
+    },
+    "Miaoshouai_Caption_Analyzer": {
+        MetaField.POSITIVE_PROMPT: {"field_name": "analyze"},
+    },
+}
