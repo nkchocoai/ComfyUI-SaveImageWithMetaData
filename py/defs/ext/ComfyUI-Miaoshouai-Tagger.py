f7b59e0f5eb98ba24e30f24e172a15b773a16441
Light-x02 <105165695+Light-x02@users.noreply.github.com>
2025-04-30 21:32:07 +0200

    Update ComfyUI-Miaoshouai-Tagger.py

--- py/defs/ext/ComfyUI-Miaoshouai-Tagger.py
+++ py/defs/ext/ComfyUI-Miaoshouai-Tagger.py
@@ -6,14 +6,8 @@ SAMPLERS = {
 }
 
 CAPTURE_FIELD_LIST = {
-    "Miaoshouai_SaveTags": {
-        MetaField.POSITIVE_PROMPT: {"field_name": "captions"},
-    },
     "Miaoshouai_Flux_CLIPTextEncode": {
         MetaField.POSITIVE_PROMPT: {"field_name": "caption"},
         MetaField.CFG:             {"field_name": "guidance"},
-    },
-    "Miaoshouai_Caption_Analyzer": {
-        MetaField.POSITIVE_PROMPT: {"field_name": "analyze"},
     },
 }
